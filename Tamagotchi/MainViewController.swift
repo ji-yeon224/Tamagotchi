a0d8a42ddb4e84ec52e1c1e474df72f969028da5
ji-yeon224 <69784492+ji-yeon224@users.noreply.github.com>
2023-08-06 18:11:24 +0900

    메세지 + 닉네임 출력하기
    메인 화면에서 경우에 따라 메세지 출력하고, 닉네임 붙이기

--- Tamagotchi/MainViewController.swift
+++ Tamagotchi/MainViewController.swift
@@ -14,6 +14,7 @@ class MainViewController: UIViewController {
     
     @IBOutlet var textImageView: UIImageView!
     
+    @IBOutlet var backVIew: UIView!
     @IBOutlet var tamaImageView: UIImageView!
     @IBOutlet var nameLabel: UILabel!
     @IBOutlet var infoLabel: UILabel!
@@ -21,25 +22,25 @@ class MainViewController: UIViewController {
     @IBOutlet var mealButton: UIButton!
     @IBOutlet var waterTextField: UITextField!
     @IBOutlet var waterButton: UIButton!
+    @IBOutlet var messageLabel: UILabel!
     
+    var level = 1
     var tamaName = ""
     var tamaInfo:TamagotchiInfo! {
         didSet{
             UserDefaults.standard.set(try? PropertyListEncoder().encode(tamaInfo), forKey: tamaInfo.name)
         }
     }
+    var userName = UserDefaults.standard.string(forKey: "userName")!
+    let messageList = Message()
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-//        let tamaList = UserDefaults.standard.array(forKey: "tamaList") as? [TamagotchiInfo] ?? [TamagotchiInfo]()
-//        print(tamaList[0])
-        
-        
+
         
         tamaInfo = getTamaInfo()
-        let userName = UserDefaults.standard.string(forKey: "userName")!
+        
         title = "\(userName)님의 다마고치"
         navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "person.circle"), style: .plain, target: self, action: #selector(showSettingView))
         
@@ -48,6 +49,20 @@ class MainViewController: UIViewController {
         setProperties()
         
     }
+    
+
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        userName = UserDefaults.standard.string(forKey: "userName")!
+        title = "\(userName)님의 다마고치"
+        var msg = messageList.growMessage.randomElement()!
+        msg = setMessageWithName(msg)
+        messageLabel.text = msg
+        
+    }
+    
+    
     
     @IBAction func mealButtonClicked(_ sender: UIButton) {
         
@@ -62,8 +77,6 @@ class MainViewController: UIViewController {
         
     }
     
-    
-    
     @IBAction func waterButtonClicked(_ sender: UIButton) {
         
         if waterTextField.text!.count == 0 {
@@ -76,18 +89,61 @@ class MainViewController: UIViewController {
         changeTamaInfo()
 
     }
+
+    
+    
+    @objc func showSettingView() {
+        let sb = UIStoryboard(name: "Main", bundle: nil)
+        let vc = sb.instantiateViewController(identifier: SettingViewController.identifier) as! SettingViewController
+        
+        navigationController?.pushViewController(vc, animated: true)
+        
+    }
+
+    
+
+}
+
+//디자인 및 설정
+extension MainViewController {
     
     func changeTamaInfo() {
         infoLabel.text = "LV\(tamaInfo.level()) | 밥알 \(tamaInfo.meal)개 | 물방울 \(tamaInfo.water)개"
-        tamaImageView.image = UIImage(named: "\(tamaInfo.imgNum)-\(tamaInfo.level())")
-        //print(tamaInfo.level())
+        var imgNum = tamaInfo.level()
+        if imgNum > 9 {
+            imgNum = 9
+        }
+        
+        tamaImageView.image = UIImage(named: "\(tamaInfo.imgNum)-\(imgNum)")
+        
+        var msg = ""
+        if level != tamaInfo.level() {
+            msg = messageList.levelUPMessage.randomElement()!
+        }else if level == 10 {
+            msg = messageList.maxMessage.randomElement()!
+        } else{
+            msg = messageList.growMessage.randomElement()!
+        }
+        msg = setMessageWithName(msg)
+        messageLabel.text = msg
+        
+    }
+    
+    //닉네임 붙여서 메세지 멘트 가져오기
+    func setMessageWithName(_ string: String) -> String {
+        if string.contains("nickname") {
+            return string.replacingOccurrences(of: "nickname", with: userName)
+        }
+        return string
     }
     
     //레이블 버튼 등등 디자인
     func setProperties() {
+        
+        view.backgroundColor = setBackgroundColor()
+        backVIew.backgroundColor = .clear
         nameLabel.text = tamaInfo?.name
-        nameLabel.font = .systemFont(ofSize: 14)
-        nameLabel.textColor = .darkGray
+        setNameLabel(label: nameLabel)
         
         tamaImageView.image = UIImage(named: "\(tamaInfo.imgNum)-1")
         tamaImageView.contentMode = .scaleAspectFill
@@ -101,6 +157,11 @@ class MainViewController: UIViewController {
         infoLabel.textColor = .darkGray
         infoLabel.font = .systemFont(ofSize: 13)
         infoLabel.textAlignment = .center
+        
+        messageLabel.font = .systemFont(ofSize: 13)
+        messageLabel.textAlignment = .center
+        messageLabel.numberOfLines = 0
+        messageLabel.textColor = .darkGray
         
         designButton(button: waterButton, image: "leaf.circle", title: "물먹기")
         designButton(button: mealButton, image: "drop.circle", title: "밥먹기")
@@ -123,45 +184,32 @@ class MainViewController: UIViewController {
         waterTextField.layer.addSublayer((border))
     }
     
-    func designButton(button: UIButton, image: String, title: String) {
-        
-        var config = UIButton.Configuration.filled()  //기본 시스템 버튼
-        config.title = title
-        config.image = UIImage(systemName: image) //기본으로 애플에 등록된 이미지
-        config.buttonSize = .small
-        config.baseForegroundColor = .darkGray
-        config.baseBackgroundColor = .clear
-        config.imagePadding = 5
-        config.imagePlacement = .leading
-        config.titleAlignment = .center
-        
-        config.cornerStyle = .capsule
-        
-        button.configuration = config //outlet에 설정 붙이기
-        
-    }
-    
-    
     func getTamaInfo() -> TamagotchiInfo {
         guard let tamaInfo = UserDefaults.standard.value(forKey: tamaName) as? Data,
               let data = try? PropertyListDecoder().decode(TamagotchiInfo.self, from: tamaInfo) else {return TamagotchiInfo(name: "", profile: "", imgNum: -1)}
         return data
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        //print(#function)
-        let userName = UserDefaults.standard.string(forKey: "userName")
-        title = "\(userName!)님의 다마고치"
-    }
-    
-    @objc func showSettingView() {
-        let sb = UIStoryboard(name: "Main", bundle: nil)
-        let vc = sb.instantiateViewController(identifier: SettingViewController.identifier) as! SettingViewController
+    func designButton(button: UIButton, image: String, title: String) {
         
-        navigationController?.pushViewController(vc, animated: true)
+        var config = UIButton.Configuration.filled()
+        config.title = title
+        config.image = UIImage(systemName: image)
+        config.buttonSize = .small
+        config.baseForegroundColor = .darkGray
+        config.baseBackgroundColor = .clear
+        config.imagePadding = 4
+        config.imagePlacement = .leading
+        
+    
+        config.titleAlignment = .center
+        
+        config.cornerStyle = .capsule
+        
+        button.configuration = config
+        button.layer.cornerRadius = 10
+        button.layer.borderColor = UIColor.darkGray.cgColor
+        button.layer.borderWidth = 1
         
     }
-
-    
-
 }
